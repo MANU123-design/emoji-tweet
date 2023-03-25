aa7be5e71b9383479695a0a367654e9770f048eb
MANU123-design <manumr15777@gmail.com>
2023-03-25 18:41:11 +0530

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,14 @@ import Head from "next/head";
 import Link from "next/link";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import { api } from "~/utils/api";
+import { divide } from "react-native-reanimated";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  
 
   const user = useUser()
+
+  const {data} = api.posts.getAll.useQuery()
 
   return (
     <>
@@ -19,6 +22,9 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div>{!user.isSignedIn && <SignInButton/>}</div>
       <div>{!!user.isSignedIn && <SignOutButton/>}</div>
+      <div>
+        {data?.map((post) => (<div key={post.id}>{post.content}</div>) )}
+      </div>
       </main>
     </>
   );
